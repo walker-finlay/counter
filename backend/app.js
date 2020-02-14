2d8e1ffd07c0ac631fea9f5b335d2c0db0cfb262
Walker Finlay <mwf319@lehigh.edu>
2020-02-14 17:13:51 -0500

    app.js to backend/

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,70 @@
+var express = require('express');
+var app = express();
+
 var db = require('./db');
+var passwordHash = require('password-hash');
 
-async function test() {
-    await console.log(await db.getCounterTable());
-    await db.updateCounter(2, 1);
-    await console.log(await db.getCounterTable());
-    db.disconnect();
-}
+// POST route for a new user
+app.post('/new_user', function (req, res) {
+    var obj = JSON.parse(req);
+    var passhash = passwordHash.generate(obj.password);
+    db.addUser(obj.name, passhash, obj.counterval);
+    res.status(200);
+    res.send("Ok");
+})
 
-test();
+// POST route for logging in
+app.post('/login', function(req, res) {
+    var obj = JSON.parse(req);
+    var hashed_pwd = passwordHash.generate(obj.password);
+    var checkPass = db.getPass(obj.u_name) // CHECK THIS !!!
+    if (checkPass == hashed_pwd) {
+        res.status(200);
+        res.send("Ok");
+    } else {
+        res.send("Invalid Login");
+    }
+    // how to login ???
+    res.status(200);
+    res.send("Ok");
+})
+
+// POST to add a new counter for existing user
+app.post('/new_counter', function(req, res) {
+    var obj = JSON.parse(req);
+    db.addCounter(obj.u_id, obj.value);
+    res.status(200);
+    res.send("Ok");
+})
+
+// GET to get the counter(s) of existing users
+app.get('/get_counters', function(req, res) {
+    var obj = JSON.parse(req);
+    var toReturn = db.getUserCounters(obj.u_id);
+    res.status(200);
+    res.json(toReturn);
+})
+
+// PUT route to increment a counter
+app.put('/increment_counter', function(req, res) {
+    var obj = JSON.parse(req);
+    db.updateCounter(obj.c_id, 1);
+    res.status(200);
+    res.send("Ok");
+})
+
+// PUT route to decrement a counter
+app.put('/decrement_counter', function(req, res) {
+    var obj = JSON.parse(req);
+    db.updateCounter(obj.c_id, -1);
+    res.status(200);
+    res.send("Ok");
+})
+
+// route to delete a users counter
+app.delete('/delete_counter', function(req, res) {
+    var obj = JSON.parse(req);
+    deleteCounter(obj.c_id);
+    res.status(200);
+    res.send("Ok");
+})
